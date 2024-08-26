af713f582e5758ec1a312b7c0cad5c6d04cebde5
pinkushgole <pinkushgole@gmail.com>
2024-08-26 20:08:05 +0530

    Update views.py

--- Main_Project/vege/views.py
+++ Main_Project/vege/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render,redirect
 from .models import Receipe
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+
 # Create your views here.
 
 def receipes(request):
@@ -62,3 +66,31 @@ def update_receipe(request,id):
 
    return render(request,"update.html",context)
    
+def register_page(request):
+    
+    if request.method=="POST":
+       first_name=request.POST.get('first_name')
+       last_name=request.POST.get('last_name')
+       username=request.POST.get('username')
+       password=request.POST.get('password')
+       
+       user=User.objects.filter(username=username)
+
+       if user.exists():
+          messages.info(request,'user name alredy exist')
+          return redirect('/register/')
+
+       user=User.objects.create(
+          first_name=first_name,
+          last_name=last_name,
+          username=username,
+       )
+       user.set_password(password)
+       user.save()
+
+       return redirect('/register/')
+
+    return render(request,"register.html")
+
+def login_page(request):
+    return render(request,"login.html")
